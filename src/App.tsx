5705e7af25296e9d1346dcdcab6276410a5f6003
abel-otegbola <abel15655@gmail.com>
2023-10-11 19:40:38 +0100

    feat: add direction functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FaChevronDown, FaChevronLeft, FaChevronRight, FaChevronUp } from "react-icons/fa"
 import "./assets/scss/index.css"
 import Topbar from "./components/topbar"
 import Tree from './components/tree'
@@ -5,13 +6,23 @@ import { useState } from "react"
 
 function App() {
   const [zoom, setZoom] = useState(90)
+  const [pos, setPos] = useState({ inline: 0, block: 0 })
 
   return (
     <div>
-      <Topbar zoom={zoom} setZoom={setZoom} />
+      <Topbar zoom={zoom} setZoom={setZoom} setPos={setPos} />
       <main>
-        <div className='canvas' style={{ zoom: zoom + "%" }}>
+        <div className='canvas' style={{ zoom: zoom + "%", 
+          transform: `translate(${pos.block}px, ${pos.inline}px)`
+        }}>
           <Tree/>
+        </div>
+
+        <div className="direction">
+          <span className="top" onClick={() => setPos({ block: pos.block, inline: pos.inline - 40 })}><FaChevronUp /></span>
+          <span className="bottom" onClick={() => setPos({ block: pos.block, inline: pos.inline + 40 })}><FaChevronDown /></span>
+          <span className="left" onClick={() => setPos({ block: pos.block - 40, inline: pos.inline })}><FaChevronLeft /></span>
+          <span className="right" onClick={() => setPos({ block: pos.block + 40, inline: pos.inline })}><FaChevronRight /></span>
         </div>
       </main>
     </div>
--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-import { FaCheck } from 'react-icons/fa'
+import { FaCheck, FaPaperPlane } from 'react-icons/fa'
 
 interface topbarProps {
     zoom: number,
+    setPos: React.Dispatch<React.SetStateAction<any>>,
     setZoom: React.Dispatch<React.SetStateAction<number>>
 }
 
-const Topbar = ({ zoom, setZoom }: topbarProps) => {
+const Topbar = ({ zoom, setZoom, setPos }: topbarProps) => {
     const [open, setOpen] = useState(false)
 
     const handleTheme = () => {
@@ -19,13 +20,14 @@ const Topbar = ({ zoom, setZoom }: topbarProps) => {
 
             <div className="buttons">
                 <p onClick={() => handleTheme()}>theme</p>
+                <p onClick={() => setPos({ inline: 0, block: 0 })}><FaPaperPlane /></p>
                 <p onClick={() => setZoom(zoom - 10)}>-</p>
                 <p onClick={() => setOpen(!open)}>
                     {zoom}%
                     <ul className={open ? "open" : ""}>
                         {
                             [25, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150].map(item => (
-                                <li className={zoom === item ? "active" : ""} onClick={() => setZoom(item)}>{item}% {zoom === item ? <FaCheck /> : ""}</li>
+                                <li key={item} className={zoom === item ? "active" : ""} onClick={() => setZoom(item)}>{item}% {zoom === item ? <FaCheck /> : ""}</li>
                             ))
                         }
                     </ul>
